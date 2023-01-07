8157059b228b6a96bad7e6be47c6d4243e840f13
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-07 17:57:37 +0100

    update routes

--- app/src/router/index.ts
+++ app/src/router/index.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 
 import Connect from "@/views/Connect.vue";
 import Dashboard from "@/views/Dashboard.vue";
+import Streams from "@/views/stream/Streams.vue";
+import CreateStream from "@/views/stream/Create.vue";
 
 const routes = [
   {
@@ -10,6 +12,8 @@ const routes = [
     children: [
       { path: "/connect", name: "Connect", component: Connect },
       { path: "/dashboard", name: "Dashboard", component: Dashboard },
+      { path: "/stream/create", name: "CreateStream", component: CreateStream },
+      { path: "/streams/:type", name: "Streams", component: Streams },
     ],
   },
 ];

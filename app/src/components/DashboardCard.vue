b09588d460bb07fb2681deb0e915bba4f6e62e93
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-09 00:26:14 +0100

    dashboard card

---
+++ app/src/components/DashboardCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-card flat border class="mx-auto py-1">
+    <v-card-text>
+      <p class="mb-4">{{ title }}</p>
+      <h1>{{ count }}</h1>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script lang="ts" setup>
+defineProps<{ count: number; title: string }>();
+</script>
--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -4,16 +4,18 @@
   </div>
 
   <v-row class="mb-3">
-    <template v-for="c in 4">
-      <v-col md="3" sm="6" cols="12">
-        <v-card flat border class="mx-auto py-1">
-          <v-card-text>
-            <h4 class="mb-3">Incoming Streams</h4>
-            <h1>30</h1>
-          </v-card-text>
-        </v-card>
-      </v-col>
-    </template>
+    <v-col md="3" sm="6" cols="12">
+      <DashboardCard
+        title="Incoming Streams"
+        :count="streamStore.getStreamsCount('incoming')"
+      />
+    </v-col>
+    <v-col md="3" sm="6" cols="12">
+      <DashboardCard
+        title="Outgoing Streams"
+        :count="streamStore.getStreamsCount('outgoing')"
+      />
+    </v-col>
   </v-row>
 
   <div class="d-flex mt-5 mb-3">
@@ -24,17 +26,16 @@
     </v-btn>
   </div>
 
-  <StreamsTable :streams="streams.all" />
+  <StreamsTable :streams="streamStore.getStreams()" />
 </template>
 
 <script setup lang="ts">
-import { storeToRefs } from "pinia";
 import { useStreamStore } from "@/store";
 import { onMounted } from "vue";
 import StreamsTable from "@/components/StreamsTable.vue";
+import DashboardCard from "@/components/DashboardCard.vue";
 
 const streamStore = useStreamStore();
-const { streams } = storeToRefs(streamStore);
 
 onMounted(async () => {
   await streamStore.fetchStreams();

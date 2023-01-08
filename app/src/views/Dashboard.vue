cdd7ff5f031470d16c43113e299c140a7a96f357
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-08 17:51:16 +0100

    update dashboard

--- app/src/views/Dashboard.vue
+++ app/src/views/Dashboard.vue
@@ -24,102 +24,23 @@
     </v-btn>
   </div>
 
-  <v-table hover>
-    <thead>
-      <tr>
-        <th class="text-uppercase" v-for="head in heads">
-          {{ head }}
-        </th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr v-for="stream in streams" :key="stream.id">
-        <td>
-          <v-btn
-            flat
-            density="compact"
-            class="pa-0"
-            color="primary"
-            variant="link"
-          >
-            {{ utils.truncateAddress(stream.id) }}
-          </v-btn>
-        </td>
-        <td>{{ stream.amount }}</td>
-        <td>
-          <v-progress-linear
-            :model-value="stream.progress"
-            color="primary"
-            striped
-          />
-        </td>
-        <td>
-          <v-btn
-            flat
-            density="compact"
-            class="pa-0"
-            color="primary"
-            variant="link"
-          >
-            {{ utils.truncateAddress(stream.receiver) }}
-          </v-btn>
-        </td>
-        <td>{{ stream.duration }}</td>
-        <td>{{ stream.status }}</td>
-        <td>
-          <v-btn
-            flat
-            density="compact"
-            class="pa-0"
-            color="primary"
-            variant="link"
-          >
-            view
-          </v-btn>
-        </td>
-      </tr>
-    </tbody>
-  </v-table>
+  <StreamsTable :streams="state.streams" />
 </template>
 
 <script setup>
-import { utils } from "@/utils";
+import { stream } from "@/services";
+import { useConnectionStore } from "@/store";
+import { onMounted, reactive } from "vue";
+import StreamsTable from "@/components/StreamsTable.vue";
 
-const heads = ["Stream ID", "Amount", "Progress", "To", "Duration", "Status"];
-const streams = [
-  {
-    id: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    amount: "3000",
-    progress: "30%",
-    receiver: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    duration: "40 Days",
-    status: "Active",
-  },
-  {
-    id: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    amount: "3000",
-    progress: "30%",
-    receiver: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    duration: "40 Days",
-    status: "Active",
-  },
-  {
-    id: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    amount: "3000",
-    progress: "30%",
-    receiver: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    duration: "40 Days",
-    status: "Active",
-  },
-  {
-    id: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    amount: "3000",
-    progress: "30%",
-    receiver: "0xa15056f2f1ca22bbdc9ecac328c4fb35d5185e01",
-    duration: "40 Days",
-    status: "Active",
-  },
-];
+const connectionStore = useConnectionStore();
+
+const state = reactive({ streams: [] });
+
+onMounted(async () => {
+  const streams = await stream.getStreams(connectionStore.address, "outgoing");
+  state.streams = streams;
+});
 </script>
 
 <style>

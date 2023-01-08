cdc95ca37f741ca61e6301a79c49494600b0747f
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-08 17:13:44 +0100

    stream service update

--- app/src/services/stream.ts
+++ app/src/services/stream.ts
@@ -1,28 +1,48 @@
 import { env } from "@/config";
 import { CreateStreamData } from "@/types";
-import { connection } from "@/utils";
+import { connection, provider } from "@/services";
+import { SuiData, SuiMoveObject } from "@mysten/sui.js";
 
 export class Stream {
   public module = "stream";
 
   async createStream(data: CreateStreamData) {
+    const now = String(Math.round(Date.now() / 1000) - 500);
     const payload = {
       function: "create_stream",
       module: this.module,
       package: env.slidePackageId,
       valueArgs: [
+        env.streamRegistryId,
         data.amount,
         data.coinId,
         data.recipient,
         data.startTime,
         data.endTime,
-        null,
-        Date.now() / 1000,
+        now,
       ],
       typeArgs: [data.coinType],
     };
 
     const response = await connection.executeMoveCall(payload);
     return response;
+  }
+
+  async getStreams(address: string, type?: "incoming" | "outgoing") {
+    const registry = await provider.getObject(env.streamRegistryId);
+    if (registry.status === "Exists") {
+      const { data } = registry.details as { data: SuiData & SuiMoveObject };
+
+      console.log(data);
+
+      if (type === "outgoing") {
+        const streams = await provider.getDynamicFieldObject(
+          data.fields.user_outgoing_streams.fields.id.id,
+          address
+        );
+
+        console.log(streams);
+      }
+    }
   }
 }

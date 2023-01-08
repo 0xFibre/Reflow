18bf7ae07488fe6325b6686130a1312a13b0148e
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-08 17:13:17 +0100

    add date and bignumber utils

---
+++ app/src/utils/bignumber.ts
@@ -0,0 +1,5 @@
+import { BigNumber } from "bignumber.js";
+
+BigNumber.config({ EXPONENTIAL_AT: [-100, 100] });
+
+export { BigNumber };
---
+++ app/src/utils/date.ts
@@ -0,0 +1,16 @@
+export const date = {
+  getHtmlDateValue(date: Date) {
+    const y = date.getFullYear();
+    const m = String(date.getMonth() + 1);
+    const d = String(date.getDate());
+
+    return `${y}-${m.length < 2 ? "0" + m : m}-${d.length < 2 ? "0" + d : d}`;
+  },
+
+  getHtmlTimeValue(date: Date) {
+    const h = String(date.getHours());
+    const m = String(date.getMinutes());
+
+    return `${h.length < 2 ? "0" + h : h}:${m.length < 2 ? "0" + m : m}`;
+  },
+};

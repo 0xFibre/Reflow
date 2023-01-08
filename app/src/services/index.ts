553f6bc3f3f1faee78c6587a25c977f54566028c
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-08 17:13:53 +0100

    update

--- app/src/services/index.ts
+++ app/src/services/index.ts
@@ -1,5 +1,9 @@
 import { Stream } from "./stream";
 import { Connection } from "./connection";
+import { Coin } from "./coin";
+import { Provider } from "./provider";
 
 export const stream = new Stream();
 export const connection = new Connection();
+export const coin = new Coin();
+export const provider = new Provider();

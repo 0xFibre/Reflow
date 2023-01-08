aaacb1c8e1c6d14c1fc7ca2c37cd2f7da770e63c
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-08 23:40:37 +0100

    add explorer url to config

--- app/src/config/config.ts
+++ app/src/config/config.ts
@@ -2,9 +2,11 @@ interface Config {
   stream: {
     status: string[];
   };
+  explorerUrl: string;
 }
 export const config: Config = {
   stream: {
     status: ["Active", "Closed"],
   },
+  explorerUrl: "https://explorer.sui.io",
 };

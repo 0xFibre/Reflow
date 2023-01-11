5a8e5091c6574d433dc8b3014ada126b957151e9
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-11 20:49:10 +0100

    update connection store

--- app/src/store/connection.ts
+++ app/src/store/connection.ts
@@ -1,18 +1,45 @@
 import { defineStore } from "pinia";
 import { useLocalStorage } from "@vueuse/core";
+import { connection } from "@/services";
 
 export const useConnectionStore = defineStore("connection", {
-  state: () =>
-    useLocalStorage<{
+  state: () => ({
+    wallets: <any[]>[],
+    connection: useLocalStorage<{
       isConnected: boolean;
       wallet: string | null;
       address: string | null;
-    }>("connection", { isConnected: false, wallet: null, address: null }),
+    }>("connection", {
+      isConnected: false,
+      wallet: null,
+      address: null,
+    }),
+  }),
+
   actions: {
-    persistConnection(address: string, wallet: string) {
-      this.address = address;
-      this.wallet = wallet;
-      this.isConnected = true;
+    async establishConnection(name: string) {
+      connection.selectAdapter(name);
+      const result = await connection.connect();
+
+      this.connection = {
+        address: result.account,
+        wallet: name,
+        isConnected: true,
+      };
+    },
+
+    async deleteConnection() {
+      await connection.disconnect();
+
+      this.connection = {
+        address: null,
+        wallet: null,
+        isConnected: false,
+      };
+    },
+
+    fetchWalletAdaptersMeta() {
+      this.wallets = <any>connection.getWalletAdaptersMeta();
     },
   },
 });

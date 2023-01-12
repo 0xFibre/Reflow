aea485c130a6ed0179a097699428d0e4b56fa9aa
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-12 10:47:16 +0100

    update app bar menu

--- app/src/components/menu/AppBarMenu.vue
+++ app/src/components/menu/AppBarMenu.vue
@@ -2,21 +2,24 @@
   <v-menu :activator="activator">
     <v-list>
       <v-list-item
-        v-for="(item, index) in menuItems"
+        v-for="(item, index) in items"
         :key="index"
         :value="index"
         :prepend-icon="item.icon"
         :title="item.title"
+        @click="item.action"
       />
     </v-list>
   </v-menu>
 </template>
 
 <script lang="ts" setup>
-defineProps<{ activator: string }>();
-
-const menuItems = [
-  { icon: "mdi-open-in-new", title: "View on explorer" },
-  { icon: "mdi-logout", title: "Disconnect" },
-];
+defineProps<{
+  activator: string;
+  items: {
+    title: string;
+    icon: string;
+    action: Function;
+  }[];
+}>();
 </script>

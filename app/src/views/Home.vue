d30e3cdce296b5a6f06cabb65f644f339c144076
bytedeveloperr <abdulrahmanyusuf125@gmail.com>
2023-01-13 11:10:50 +0100

    update landing page

--- app/src/views/Home.vue
+++ app/src/views/Home.vue
@@ -1,9 +1,31 @@
 <template>
+  <v-app-bar flat>
+    <v-container class="fill-height d-flex align-center">
+      <v-app-bar-nav-icon
+        class="d-block d-lg-none"
+        variant="text"
+        @click.stop="drawer = !drawer"
+      />
+
+      <v-app-bar-title class="no-select">
+        <h3>Reflow</h3>
+      </v-app-bar-title>
+
+      <v-spacer />
+
+      <v-btn variant="text">Go to dashboard</v-btn>
+    </v-container>
+  </v-app-bar>
+
+  <v-navigation-drawer v-model="drawer" location="bottom" temporary>
+    <v-list :items="items"></v-list>
+  </v-navigation-drawer>
+
   <v-container>
     <v-sheet class="mt-12">
       <v-row>
         <v-col cols="12" sm="6">
-          <h3 class="text-h3 font-weight-bold fonted">
+          <h3 class="text-h3 mb-3 font-weight-bold fonted">
             Real-time value streaming protocol on Sui
           </h3>
           <p class="my-2 text-subtitle-1 fonted">
@@ -12,7 +34,9 @@
           </p>
 
           <div class="my-10">
-            <v-btn flat variant="flat" color="primary">Start Streaming</v-btn>
+            <v-btn flat variant="flat" color="primary" to="/dashboard">
+              Start Streaming
+            </v-btn>
           </div>
         </v-col>
         <v-col cols="12" sm="6">
@@ -28,15 +52,13 @@
       </v-row>
     </v-sheet>
 
-    <h4 class="text-h4 text-center font-weight-bold my-5 fonted">
-      Why Reflow?
-    </h4>
+    <h5 class="text-h5 mt-10 font-weight-bold fonted">Why Reflow?</h5>
 
-    <v-row>
+    <v-row class="mt-5">
       <template v-for="(data, i) in sect" :key="i">
-        <v-col cols="12" sm="6">
+        <v-col class="my-0" cols="12" sm="6">
           <div class="d-flex">
-            <v-avatar variant="tonal" color="info" class="me-3">
+            <v-avatar variant="tonal" color="secondary" class="me-3">
               <v-icon :icon="data.icon" />
             </v-avatar>
 
@@ -49,11 +71,9 @@
       </template>
     </v-row>
 
-    <h4 class="text-h4 text-center font-weight-bold mt-15 fonted">
-      Build on Reflow
-    </h4>
+    <h5 class="text-h5 mt-10 font-weight-bold fonted">Build on Reflow</h5>
 
-    <v-sheet class="my-5">
+    <v-sheet class="mt-5">
       <v-row>
         <v-col cols="12" sm="8" md="6">
           <p class="mb-5">
@@ -83,7 +103,7 @@
     </v-sheet>
   </v-container>
 
-  <v-sheet class="pt-3" color="secondary">
+  <v-sheet class="pt-3 mt-5" color="secondary">
     <v-container>
       <div class="mb-5">
         <h6 class="text-h6 font-weight-bold fonted">Reflow</h6>
@@ -114,6 +134,28 @@
 </template>
 
 <script lang="ts" setup>
+import { ref, Ref } from "vue";
+
+const drawer: Ref<boolean | null> = ref(null);
+const items = [
+  {
+    title: "Foo",
+    value: "foo",
+  },
+  {
+    title: "Bar",
+    value: "bar",
+  },
+  {
+    title: "Fizz",
+    value: "fizz",
+  },
+  {
+    title: "Buzz",
+    value: "buzz",
+  },
+];
+
 const sect = [
   {
     icon: "mdi-reload",
@@ -173,7 +215,7 @@ const usecases = [
 
 <style>
 .fonted {
-  font-family: Nunito !important;
+  font-family: Circular !important;
 }
 
 ul {
